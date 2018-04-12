d307f22e8f6aa8134a998023be77b321874e140f
AnthonyChamberlin <antchamberlin.js@gmail.com>
2018-04-12 19:51:48 +0100

    add files

---
+++ main.js
@@ -0,0 +1,23 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "main.json";
+
+xmlhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+var myArr = JSON.parse(this.responseText);
+    myFunction(myArr);
+    }
+};
+
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+
+function myFunction(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<li class="list-group-item">' + arr[i].name + ' is a ' + arr[i].role + ' at ' + arr[i].company + '</li>'; 
+        
+    }
+    document.getElementById("employees").innerHTML = out;
+}
